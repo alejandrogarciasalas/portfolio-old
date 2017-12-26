541787e5c943abf066a5cf0623730861d129267a
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2017-12-26 18:33:10 -0500

    WIP

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
-$('.grid').isotope({
+var $grid = $('.grid').isotope({
   itemSelector: '.grid-item',
   masonry: {
   }
+});
+
+// filter items on button click
+$('.filter').on( 'click', 'a', function() {
+  event.preventDefault();
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
 });
