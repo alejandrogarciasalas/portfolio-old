e5c7803186db6f00043a33cc449aead31a27eaa4
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2017-12-26 19:58:24 -0500

    refactor js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,30 @@
-var $grid = $('.grid').isotope({
-  itemSelector: '.grid-item',
-  masonry: {
-  }
-});
+function init_isotope_grid() {
+  var $grid = $('.grid').isotope({
+    itemSelector: '.grid-item',
+    masonry: {
+    }
+  });
 
-// filter items on button click
-$('.filter').on( 'click', 'a', function() {
-  event.preventDefault();
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
+  // filter items on button click
+  $('.filter').on( 'click', 'a', function() {
+    event.preventDefault();
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+}
+
+function init_tilter() {   
+   var idx = 0;   
+   [].slice.call(document.querySelectorAll('a.tilter')).forEach(function(el, pos) {     
+     idx = pos%2 === 0 ? idx+1 : idx;   
+     new TiltFx(el);    
+   });    
+}
+
+
+$('.tilter__figure').imagesLoaded( { background: true }, function() {
+  $('.spinner').remove();
+  $('main').removeClass('hidden');
+  init_tilter();
+  init_isotope_grid();
 });
